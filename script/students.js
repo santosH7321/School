96edcff4c001952ad229d807c855dc773d1b4ee6
Santosh Kumar <santoshkumar23kky@gmail.com>
2025-06-23 11:34:01 +0530

    new admission form section

---
+++ script/students.js
@@ -0,0 +1,21 @@
+const takeAdmission = (e) => {
+    e.preventDefault();
+    const studentName = document.getElementById('studentName').value.trim();
+    const fatherName = document.getElementById('fatherName').value.trim();
+    const motherName = document.getElementById('motherName').value.trim();
+    const gender = document.getElementById('gender').value.trim();
+    const dob = document.getElementById('dob').value.trim();
+    const religion = document.getElementById('religion').value.trim();
+    const mobile = document.getElementById('mobile').value.trim();
+    const email = document.getElementById('email').value.trim();
+    const classes = document.getElementById('class').value.trim();
+    const section = document.getElementById('section').value.trim();
+    const address = document.getElementById('address').value.trim();
+    const city = document.getElementById('city').value.trim();
+    const state = document.getElementById('state').value.trim();
+    const country = document.getElementById('country').value.trim();
+    const pinCode = document.getElementById('pinCode').value.trim();
+    const previousSchool = document.getElementById('previousSchool').value.trim();
+
+    console.log(studentName, fatherName, motherName, gender, dob, religion, mobile, email, classes, section, address, city, state, country, pinCode, previousSchool);
+}

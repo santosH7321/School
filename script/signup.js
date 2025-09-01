40a7a92e212cf01bd5e795ecbee90da2cbdfe893
Santosh Kumar <santoshkumar23kky@gmail.com>
2025-09-01 14:05:40 +0530

    signup connected with server

--- script/signup.js
+++ script/signup.js
@@ -1,9 +1,32 @@
 const signup = (e) => {
     e.preventDefault();
-    const fullname = document.getElementById('fullname').value.trim();
+    const schoolName = document.getElementById('schoolName').value.trim();
+    const dirName = document.getElementById('dirName').value.trim();
     const mobile = document.getElementById('mobile').value.trim();
     const email = document.getElementById('email').value.trim();
     const password = document.getElementById('password').value.trim();
 
-    console.log(`Full Name: ${fullname}, Mobile: ${mobile}, Email: ${email}, Password: ${password}`);
+    const payload = {
+        schoolName: schoolName,
+        dirName: dirName,
+        mobile: mobile,
+        email: email,
+        password: password
+    }
+    axios.post("http://localhost:8080/school/signup", payload)
+    .then((res) => {
+        new Swal({
+            icon: "success",
+            title: res.data.message,
+        })
+        .then(() => {
+            location.href = '/login.html'
+        })
+    })
+    .catch((err) => {
+        new Swal({
+            icon: "error",
+            title: "Signup failed"
+        })
+    })
 }
